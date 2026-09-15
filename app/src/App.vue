ad9c00a60724ddce1096d8801ba475dbc3760d59
Jeyker Salinas <jeyker.salinas13@gmail.com>
2026-09-15 16:49:08 +0200

    posthog integration

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { useStorage } from '@vueuse/core'
+import posthog from 'posthog-js'
 
 import DemoPrivacyNotice from '@/components/privacy/DemoPrivacyNotice.vue'
 import { useLocale } from '@/composables/useLocale'
 
+const posthogConfigured = Boolean(
+  import.meta.env.VITE_POSTHOG_PROJECT_TOKEN && import.meta.env.VITE_POSTHOG_HOST,
+)
 const { uiLocale } = useLocale()
 const hasAcknowledgedDemoNotice = useStorage(
   'django-demo-notice-v1-acknowledged',
   false,
 )
+
+function acceptDemoPrivacyNotice() {
+  if (posthogConfigured) posthog.capture('demo_privacy_notice_accepted')
+  hasAcknowledgedDemoNotice.value = true
+}
 </script>
 
 <template>
@@ -17,7 +26,7 @@ const hasAcknowledgedDemoNotice = useStorage(
     <RouterView />
     <DemoPrivacyNotice
       :open="!hasAcknowledgedDemoNotice"
-      @accept="hasAcknowledgedDemoNotice = true"
+      @accept="acceptDemoPrivacyNotice"
     />
   </UApp>
 </template>
